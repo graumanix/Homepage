a165a97fe275694829b03540d519b91e075db873
graumanix <graumanix@gmail.com>
2020-08-29 22:23:40 +0200

    v0.4

--- darkmode.js
+++ darkmode.js
@@ -4,16 +4,12 @@ let dark = false;
     document.querySelector('#dark').onclick = function() {
         if (dark == false)
         {
-            document.getElementById("#dark").classList.remove('btn-dark');
-            document.getElementById("#dark").classList.add('btn-light');
             body.style.backgroundColor = "rgb(53, 53, 53)";
             document.querySelector('#dark').innerHTML = "Light";
             dark = true;
         }
         else
         {
-            document.getElementById("#dark").classList.remove('btn-light');
-            document.getElementById("#dark").classList.add('btn-dark');
             body.style.backgroundColor = "rgb(220, 220, 220)";
             document.querySelector('#dark').innerHTML = "Dark";
             dark = false;
