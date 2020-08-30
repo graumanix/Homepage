1f34af7f0780ddff4e069f06e160671c3cbbc963
graumanix <graumanix@gmail.com>
2020-08-30 13:53:52 +0200

    v0.7

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,5 @@
 let dark = false;
     let body = document.querySelector('body');
-    let footer = document.querySelector('.footer');
 
     document.querySelector('#dark').onclick = function() {
         if (dark == false)
@@ -8,8 +7,9 @@ let dark = false;
             body.style.backgroundColor = "rgb(70, 70, 80)";
             footer.style.backgroundColor = "rgb(30, 30, 40)";
             document.querySelector('#dark').innerHTML = "Light";
-            document.querySelector('#dark').className = "btn btn-light right"
-            document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark"
+            document.querySelector('#dark').className = "btn btn-light right";
+            document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
+            document.querySelector('#footer').style.backgroundColor = "rgb(30, 30, 40)";
             dark = true;
         }
         else
@@ -19,6 +19,7 @@ let dark = false;
             document.querySelector('#dark').innerHTML = "Dark";
             document.querySelector('#dark').className = "btn btn-dark right"
             document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light"
+            document.querySelector('#footer').style.backgroundColor = "rgb(180, 180, 190)";
             dark = false;
         }
     }
