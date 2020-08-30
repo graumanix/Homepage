2fe8459dc91809ffa90f56233f75133906f05782
graumanix <graumanix@gmail.com>
2020-08-30 15:58:40 +0200

    v0.7

--- darkmode.js
+++ darkmode.js
@@ -1,21 +1,21 @@
 document.querySelector('#dark').onclick = function() 
 {
-    if (dark == false)
+    if (dark == true)
     {
         document.querySelector('#dark').className = "btn btn-light right";
         document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
         document.getElementById('theme').setAttribute('href', 'darkmode.css');
         localStorage.setItem("sheet", 'darkmode.css');
-        dark = true;
+        dark = false;
         localStorage.setItem("dark", dark);
     }
-    else if (dark == true)
+    else if (dark == false)
     {
         document.querySelector('#dark').className = "btn btn-dark right";
         document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light";
         document.getElementById('theme').setAttribute('href', 'light.css');
         localStorage.setItem("sheet", 'light.css');
-        dark = false;
+        dark = true;
         localStorage.setItem("dark", dark);
     }
     else
@@ -24,7 +24,7 @@ document.querySelector('#dark').onclick = function()
         document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
         document.getElementById('theme').setAttribute('href', 'darkmode.css');
         localStorage.setItem("sheet", 'darkmode.css');
-        dark = true;
+        dark = false;
         localStorage.setItem("dark", dark);
     }
 }
