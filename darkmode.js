06ef2e8c5d544f10e68e1e8ef0cc63290a38b4bd
graumanix <graumanix@gmail.com>
2020-08-30 14:47:40 +0200

    v0.7

--- darkmode.js
+++ darkmode.js
@@ -1,40 +1,16 @@
 let dark = false;
-    let body = document.querySelector('body');
-    body.style.backgroundColor = "rgb(240, 240, 250)";
-    document.querySelector('#dark').innerHTML = "Dark";
-    document.querySelector('#dark').className = "btn btn-dark right";
-    document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light";
-    document.querySelector('.footer').style.backgroundColor = "rgb(180, 180, 190)";
-    document.querySelector('.footer').style.color = "gray";
-    document.querySelector('.item').style.backgroundColor = "rgb(220, 220, 230)";
-    document.querySelector('.item').style.color = "black";
-    document.querySelector('.title').style.color = "black";
-
-    document.querySelector('#dark').onclick = function() {
-        if (dark == false)
-        {
-            body.style.backgroundColor = "rgb(70, 70, 80)";
-            document.querySelector('#dark').innerHTML = "Light";
-            document.querySelector('#dark').className = "btn btn-light right";
-            document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
-            document.querySelector('.footer').style.backgroundColor = "rgb(30, 30, 40)";
-            document.querySelector('.footer').style.color = "rgb(200, 200, 210)";
-            document.querySelector('.item').style.backgroundColor = "rgb(50, 50, 60)";
-            document.querySelector('.item').style.color = "white";
-            document.querySelector('.title').style.color = "white";
-            dark = true;
-        }
-        else
-        {
-            body.style.backgroundColor = "rgb(240, 240, 250)";
-            document.querySelector('#dark').innerHTML = "Dark";
-            document.querySelector('#dark').className = "btn btn-dark right";
-            document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light";
-            document.querySelector('.footer').style.backgroundColor = "rgb(180, 180, 190)";
-            document.querySelector('.footer').style.color = "gray";
-            document.querySelector('.item').style.backgroundColor = "rgb(220, 220, 230)";
-            document.querySelector('.item').style.color = "black";
-            document.querySelector('.title').style.color = "black";
-            dark = false;
-        }
+let body = document.querySelector('body');
+  
+document.querySelector('#dark').onclick = function() 
+{
+    if (dark == false)
+    {
+        
+        dark = true;
     }
+    else
+    {
+        
+        dark = false;
+    }
+}
