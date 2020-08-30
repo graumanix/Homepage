51a80e521513273bb803984d695104c6a4a48b48
graumanix <graumanix@gmail.com>
2020-08-30 15:27:45 +0200

    v0.7

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,14 @@
 let dark = false;
-let body = document.querySelector('body');
+if (dark == true)
+    {
+        document.querySelector('#dark').className = "btn btn-light right";
+        document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
+    }
+    else
+    {
+        document.querySelector('#dark').className = "btn btn-dark right";
+        document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light";
+    }
   
 document.querySelector('#dark').onclick = function() 
 {
@@ -25,16 +34,6 @@ function swapsheet(sheet)
 {
     document.getElementById('theme').setAttribute('href', sheet);
     localStorage.setItem("sheet", sheet);
-    if (dark == true)
-    {
-        document.querySelector('#dark').className = "btn btn-light right";
-        document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
-    }
-    else
-    {
-        document.querySelector('#dark').className = "btn btn-dark right";
-        document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light";
-    }
 }
 
 window.onload = _ => swapsheet(localStorage.getItem("sheet") || "default.css");
