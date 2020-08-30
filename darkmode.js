49cdf215188cbfa7e70d136491d29cc8a898739e
graumanix <graumanix@gmail.com>
2020-08-30 13:58:10 +0200

    v0.7

--- darkmode.js
+++ darkmode.js
@@ -10,16 +10,26 @@ let dark = false;
             document.querySelector('#dark').className = "btn btn-light right";
             document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-dark bg-dark";
             document.querySelector('#footer').style.backgroundColor = "rgb(30, 30, 40)";
+            items = document.querySelector('.item');
+            for (var i = 0; i < elements.length; i++) 
+            {
+                items[i].style.backgroundColor="rgb(50, 50, 60)";
+            }
             dark = true;
         }
         else
         {
-            body.style.backgroundColor = "rgb(240, 240, 240)";
+            body.style.backgroundColor = "rgb(240, 240, 250)";
             footer.style.backgroundColor = "rgb(180, 180, 190)";
             document.querySelector('#dark').innerHTML = "Dark";
             document.querySelector('#dark').className = "btn btn-dark right"
             document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light"
             document.querySelector('#footer').style.backgroundColor = "rgb(180, 180, 190)";
+            items = document.querySelector('.item');
+            for (var i = 0; i < elements.length; i++) 
+            {
+                items[i].style.backgroundColor="rgb(220, 220, 230)";
+            }
             dark = false;
         }
     }
