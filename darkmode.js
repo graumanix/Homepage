ce3fa31cb7f5bb86ac99acc8b97135fdbf3eb3ac
graumanix <graumanix@gmail.com>
2020-08-30 14:18:22 +0200

    v0..7

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,14 @@
 let dark = false;
     let body = document.querySelector('body');
+    body.style.backgroundColor = "rgb(240, 240, 250)";
+    document.querySelector('#dark').innerHTML = "Dark";
+    document.querySelector('#dark').className = "btn btn-dark right";
+    document.querySelector('#nav').className = "navbar navbar-expand-lg navbar-light bg-light";
+    document.querySelector('.footer').style.backgroundColor = "rgb(180, 180, 190)";
+    document.querySelector('.footer').style.color = "gray";
+    document.querySelector('.item').style.backgroundColor = "rgb(220, 220, 230)";
+    document.querySelector('.item').style.color = "black";
+    document.querySelector('.title').style.color = "black";
 
     document.querySelector('#dark').onclick = function() {
         if (dark == false)
